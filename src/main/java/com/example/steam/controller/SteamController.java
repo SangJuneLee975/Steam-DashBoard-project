4b6a4ccdccef7b3528bdfea479723855e13d98c7
SangJuneLee975 <pkok975@gmail.com>
2024-08-09 23:41:50 +0900

    commit test

--- src/main/java/com/example/steam/controller/SteamController.java
+++ src/main/java/com/example/steam/controller/SteamController.java
@@ -53,7 +53,7 @@ public class SteamController {
         return steamService.getAllGameStats(steamId);
     }
 
-    // 스팀 프로필을 가져오는 엔드포인트
+    // 스팀 프로필을 가져오는 메소드
     @GetMapping("/profile")
     public SteamUser getSteamProfile(Authentication authentication) {
         CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
